28536e4f0998ff602652915ae585b87ef7845fea
Diogo Dias da Silva <diasdiogo429@gmail.com>
2025-02-06 02:29:06 -0300

    Change the api message - again

--- application/main.py
+++ application/main.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Diogo passou por aqui ^^ !"}
+    assert response.json() == {"message": "Teste ^^!"}
