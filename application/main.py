a8ad138b8eb2b1ea6d1619129969155c911571d7
Diogo Dias da Silva <diasdiogo429@gmail.com>
2025-02-06 02:32:26 -0300

    Fix code error

--- application/main.py
+++ application/main.py
@@ -1,10 +1,8 @@
-from fastapi.testclient import TestClient
-from application.main import app
+from fastapi import FastAPI
 
-client = TestClient(app)
+app = FastAPI()
 
 
-def test_read_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Teste ^^!"}
+@app.get("/")
+async def root():
+    return {"message": "Teste ^^!"}
